344061235fb00e68e08d6bbf650baf25500ce62f
HariO7 <hariodukkathil@gmail.com>
2023-07-19 13:28:20 +0530

    login and register

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Login from "./pages/Login/Login";
+import Register from "./pages/Register/Register";
 
 function App() {
+
+  const router = createBrowserRouter([
+    {
+      path:"/login",
+      element:<Login />
+    },
+    {
+      path:"/Register",
+      element:<Register />
+    }
+  ])
   return (
     <div>
-      <Login />
+      <RouterProvider router={router} />
     </div>
   );
 }
